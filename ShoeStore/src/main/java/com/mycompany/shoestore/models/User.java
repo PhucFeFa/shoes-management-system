3c4001b856d111a3e08d137de5c26e448e10dc6f
Khoa169 <KhoaPDDCE181583@fpt.edu.vn>
2026-06-19 10:56:51 +0700

    Update Order Status
    Change status account

--- ShoeStore/src/main/java/com/mycompany/shoestore/models/User.java
+++ ShoeStore/src/main/java/com/mycompany/shoestore/models/User.java
@@ -11,6 +11,7 @@ public class User {
     private String fullName;
     private Timestamp createdAt;
     private String roleName;
+    private String status;
 
     public User() {
     }
@@ -35,4 +36,7 @@ public class User {
 
     public String getRoleName() { return roleName; }
     public void setRoleName(String roleName) { this.roleName = roleName; }
+    
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
 }
